dfe0d799f06e48e8ba634bd7e60501ad3feb7585
e-henry <e-henry@users.noreply.github.com>
2019-01-06 17:23:40 +0100

    Add gitlab account

--- test/sspk.js
+++ test/sspk.js
@@ -18,6 +18,7 @@ tap.test('file loading', (t) => {
   let wanted = [{
     ssh_pub_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC22UDTnx2+UvsE96VGNFq47U9oZgy4pv0BUhNhw0JbyNlGp5M+q2g0bjvIvHNS2r2+QubayZCQZ8GoPWUPEBIy2XbwHGQgasQsGaYvIB175p6fzjINANhTHzBYBFViTZPKvDBBV/qwFa/yv63QYOWl39int5rt9g7rnLeKhYuNiakat0yCghA5shxcrNF3ElcReY3w/EKg1iBaHqIfzN+ziqZVS3NL7/0ztZgKX+weGp7eltC1XC/EnYy1yS2xyOMb3o11m+FamrK9RZ4nI3o06B8yc8Dl3IYssWhDzLb1S/zgkzMdWPR8dUGlwzIQqXbwQhnE6YcQyPLP9T8iI8SskAF4oBaIfQwavfJTa+T3hNX/NqWfqU6GcU+BV6wegb3K2KiJ7xdpQTXyKE6hGYPJB3jdWVVB0R+m4k2of9c6AD/v++Ezs/S2hcKAWsRgrqomqz/XH0OizuMZZtWsh8WLO/d4ycs2jUauSgJSUZcUhAMTeAvDaxQSSxqj8E4dgP1FjQmzVxhnd/9kaL8prLmjYjD5AqNrT+EQ0TxunBbmyl3Oc8ueLbNHvBUI/YMMDWlSjpWKwFyFncie3i8+BlDFn4/5g2DABkOUHArl4YxuO8k2734U/FHUcSNQTw4xnyZKD9kRB7WPUv25KpJyAQP3BamhYquisf1GEJygyBR5iQ== me@mycomputer',
     github_account: 'e-henry',
+    gitlab_account: 'e-henry',
     name: 'Me'
   }]
   let found = app.loadStudents('test/OK.yml')
@@ -26,6 +27,7 @@ tap.test('file loading', (t) => {
   let notWanted = [{
     ssh_pub_key: 'ssh-rsa AAAAB3zaC1yc2EAAAADAQABAAACAQC22UDTnx2+UvsE96VGNFq47U9oZgy4pv0BUhNhw0JbyNlGp5M+q2g0bjvIvHNS2r2+QubayZCQZ8GoPWUPEBIy2XbwHGQgasQsGaYvIB175p6fzjINANhTHzBYBFViTZPKvDBBV/qwFa/yv63QYOWl39int5rt9g7rnLeKhYuNiakat0yCghA5shxcrNF3ElcReY3w/EKg1iBaHqIfzN+ziqZVS3NL7/0ztZgKX+weGp7eltC1XC/EnYy1yS2xyOMb3o11m+FamrK9RZ4nI3o06B8yc8Dl3IYssWhDzLb1S/zgkzMdWPR8dUGlwzIQqXbwQhnE6YcQyPLP9T8iI8SskAF4oBaIfQwavfJTa+T3hNX/NqWfqU6GcU+BV6wegb3K2KiJ7xdpQTXyKE6hGYPJB3jdWVVB0R+m4k2of9c6AD/v++Ezs/S2hcKAWsRgrqomqz/XH0OizuMZZtWsh8WLO/d4ycs2jUauSgJSUZcUhAMTeAvDaxQSSxqj8E4dgP1FjQmzVxhnd/9kaL8prLmjYjD5AqNrT+EQ0TxunBbmyl3Oc8ueLbNHvBUI/YMMDWlSjpWKwFyFncie3i8+BlDFn4/5g2DABkOUHArl4YxuO8k2734U/FHUcSNQTw4xnyZKD9kRB7WPUv25KpJyAQP3BamhYquisf1GEJygyBR5iQ== me@mycomputer',
     github_account: 'e-henry',
+    gitlab_account: 'e-henry',
     name: 'Me'
   }]
   t.notSame(found, notWanted, 'Check that an error in the file is detected')
@@ -37,6 +39,7 @@ tap.test('Validating entries', (t) => {
   let good = {
     ssh_pub_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC22UDTnx2+UvsE96VGNFq47U9oZgy4pv0BUhNhw0JbyNlGp5M+q2g0bjvIvHNS2r2+QubayZCQZ8GoPWUPEBIy2XbwHGQgasQsGaYvIB175p6fzjINANhTHzBYBFViTZPKvDBBV/qwFa/yv63QYOWl39int5rt9g7rnLeKhYuNiakat0yCghA5shxcrNF3ElcReY3w/EKg1iBaHqIfzN+ziqZVS3NL7/0ztZgKX+weGp7eltC1XC/EnYy1yS2xyOMb3o11m+FamrK9RZ4nI3o06B8yc8Dl3IYssWhDzLb1S/zgkzMdWPR8dUGlwzIQqXbwQhnE6YcQyPLP9T8iI8SskAF4oBaIfQwavfJTa+T3hNX/NqWfqU6GcU+BV6wegb3K2KiJ7xdpQTXyKE6hGYPJB3jdWVVB0R+m4k2of9c6AD/v++Ezs/S2hcKAWsRgrqomqz/XH0OizuMZZtWsh8WLO/d4ycs2jUauSgJSUZcUhAMTeAvDaxQSSxqj8E4dgP1FjQmzVxhnd/9kaL8prLmjYjD5AqNrT+EQ0TxunBbmyl3Oc8ueLbNHvBUI/YMMDWlSjpWKwFyFncie3i8+BlDFn4/5g2DABkOUHArl4YxuO8k2734U/FHUcSNQTw4xnyZKD9kRB7WPUv25KpJyAQP3BamhYquisf1GEJygyBR5iQ== me@mycomputer',
     github_account: 'e-henry',
+    gitlab_account: 'e-henry',
     name: 'Moi'
   }
   t.doesNotThrow(() => {
@@ -49,6 +52,7 @@ tap.test('Validating entries', (t) => {
     let bad = {
       ssh_pub_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC22UDTnx2+UvsE96VGNFq47U9oZgy4pv0BUhNhw0JbyNlGp5M+q2g0bjvIvHNS2r2+QubayZCQZ8GoPWUPEBIy2XbwHGQgasQsGaYvIB175p6fzjINANhTHzBYBFViTZPKvDBBV/qwFa/yv63QYOWl39int5rt9g7rnLeKhYuNiakat0yCghA5shxcrNF3ElcReY3w/EKg1iBaHqIfzN+ziqZVS3NL7/0ztZgKX+weGp7eltC1XC/EnYy1yS2xyOMb3o11m+FamrK9RZ4nI3o06B8yc8Dl3IYssWhDzLb1S/zgkzMdWPR8dUGlwzIQqXbwQhnE6YcQyPLP9T8iI8SskAF4oBaIfQwavfJTa+T3hNX/NqWfqU6GcU+BV6wegb3K2KiJ7xdpQTXyKE6hGYPJB3jdWVVB0R+m4k2of9c6AD/v++Ezs/S2hcKAWsRgrqomqz/XH0OizuMZZtWsh8WLO/d4ycs2jUauSgJSUZcUhAMTeAvDaxQSSxqj8E4dgP1FjQmzVxhnd/9kaL8prLmjYjD5AqNrT+EQ0TxunBbmyl3Oc8ueLbNHvBUI/YMMDWlSjpWKwFyFncie3i8+BlDFn4/5g2DABkOUHArl4YxuO8k2734U/FHUcSNQTw4xnyZKD9kRB7WPUv25KpJyAQP3BamhYquisf1GEJygyBR5iQ== me@mycomputer',
       github_account: 'e-henry',
+      gitlab_account: 'e-henry',
       name: 'Me'
     }
     app.verifySchema(bad)
